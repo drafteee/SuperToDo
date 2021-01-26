89f39fac3ca7ec7d15e3cd2ac92e9928c8c382f3
drafteee <sergej_-96@mail.ru>
2021-01-26 13:18:05 +0300

    #STD-65

--- django-react/django-react/settings.py
+++ django-react/django-react/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads.apps.LeadsConfig',
-    'corsheaders',  
+    'corsheaders',
     'rest_framework',
     'todo',
 ]
